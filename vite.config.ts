b302600210b4675eaaed39aac2a12511c01d5795
beta <yukun.zk@foxmail.com>
2026-07-27 10:06:44 +0800

    修改脚本描述

--- vite.config.ts
+++ vite.config.ts
@@ -6,10 +6,10 @@ export default defineConfig({
     monkey({
       entry: 'src/main.ts',
       userscript: {
-        name: 'BIMTK 数据合并拦截器 (A/C同步-V12终局生产版)',
+        name: 'BIMTK 增强',
         namespace: 'http://tampermonkey.net/',
-        version: '4.0',
-        description: '动态窃取标头跨域拉取C请求，修正State_Name字段映射并无感注入',
+        version: '1.0',
+        description: 'BIMTK 增强脚本 by 江西华泓精工',
         match: ['*://*.bimtk.com/*'],
         grant: ['GM_xmlhttpRequest'],
         connect: ['integ-plat-proj-api.bimtk.com'],
