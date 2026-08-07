b4d6f0139455ea5eb213c373b29b124d7b45df97
Beta <59674700+Beta-zk@users.noreply.github.com>
2026-08-07 09:58:47 +0800

    新增配置

--- vite.config.ts
+++ vite.config.ts
@@ -1,19 +1,22 @@
-import { defineConfig } from 'vite';
-import monkey from 'vite-plugin-monkey';
+import { defineConfig } from "vite";
+import monkey from "vite-plugin-monkey";
 
 export default defineConfig({
   plugins: [
     monkey({
-      entry: 'src/main.ts',
+      entry: "src/main.ts",
       userscript: {
-        name: 'HHJGBIM_Enhance',
-        namespace: 'http://tampermonkey.net/',
-        version: '0.1.4',
-        description: '华泓精工比姆泰克增强脚本 by 江西华泓精工',
-        match: ['*://*.bimtk.com/*'],
-        grant: ['GM_xmlhttpRequest'],
-        connect: ['integ-plat-proj-api.bimtk.com'],
-        'run-at': 'document-start',
+        name: "HHJGBIM_Enhance",
+        namespace: "http://tampermonkey.net/",
+        version: "0.1.4",
+        description: "华泓精工比姆泰克增强脚本 by 江西华泓精工",
+        match: ["*://*.bimtk.com/*"],
+        grant: ["GM_xmlhttpRequest"],
+        connect: [
+          "integ-plat-proj-api.bimtk.com",
+          "integ-plat-produce-api.bimtk.com",
+        ],
+        "run-at": "document-start",
       },
       build: {
         externalGlobals: {},
